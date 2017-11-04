9c3141d642a8e700bf78fa522bac056f503fa936
barkar19 <barkar19@gmail.com>
2017-11-04 16:08:53 +0100

    ILA tests main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,49 +6,61 @@
 #include "cilarulesextractor.h"
 #include <ctime>
 
-
 using namespace std;
 
 int main()
 {
     std::srand(std::time(0));
     std::vector<std::pair<string,string>> dataString;
-//    dataString.push_back( pair<string,string>( "data/ila.data", "%s %s %s %s" ) );
-//    dataString.push_back( pair<string,string>( "data/seasons.data", "%s %s %s %s" ) );
-    dataString.push_back( pair<string,string>( "data/iris.data", "%f %f %f %f %s" ) );
-//    dataString.push_back( pair<string,string>( "data/seeds.data", "%f %f %f %f %f %f %f %s" ) );
-//    dataString.push_back( pair<string,string>( "data/ecoli.data", "%s %f %f %f %f %f %f %f %s" ) );
-//    dataString.push_back( pair<string,string>( "data/ukm.data", "%f %f %f %f %f %s" ) );
+//    dataString.push_back( pair<string,string>( "data/iris.data", "%f %f %f %f %s" ) );
+    dataString.push_back( pair<string,string>( "data/seeds.data", "%f %f %f %f %f %f %f %s" ) );
+    dataString.push_back( pair<string,string>( "data/ecoli.data", "%s %f %f %f %f %f %f %f %s" ) );
+    dataString.push_back( pair<string,string>( "data/ukm.data", "%f %f %f %f %f %s" ) );
 //    dataString.push_back( pair<string,string>( "data/heart.data", "%f %s %s %f %f %s %s %f %s %f %s %s %f %s" ) );
-//    dataString.push_back( pair<string,string>( "data/wine.data", "%f %f %f %f %f %f %f %f %f %f %f %f %f %s" ) );
-
 
     for ( auto data : dataString )
     {
         CDataSet* pDataSet = new CDataSet;
         pDataSet->Load( data.first, data.second, ',' );
-//        std::cout << *pDataSet;
-        for ( unsigned i = 2; i <= 10; ++i )
+        for ( unsigned i = 2; i <= 10; i+=2 )
         {
+            CILARulesExtractor* pClassifier = new CILARulesExtractor();
+
             vector<CDataSet::EDiscretizationType> dis = { CDataSet::DISCRETIZATION_INTERVAL,
                                                           CDataSet::DISCRETIZATION_FREQUENCY };
+
             for ( auto t : dis )
             {
                 pDataSet->Discretize( t, i);
-                std::cerr << data.first << ",discrete"<<t<<"," << i <<"\n";
-                CILARulesExtractor ila;
+
+                std::cerr << data.first << ",discrete"<<t<<",cross,w," << i <<"\n";
 
                 CEvaluator::SetAverageMode( CEvaluator::AVG_WEIGHTED );
-                std::cout << CEvaluator::StratifiedCrossValidation( &ila, *pDataSet );
+                std::cout << data.first << ",discrete"<<t<<",cross,w," << i <<",";
+                std::cout << CEvaluator::CrossValidation( pClassifier, *pDataSet );
                 CEvaluator::SetAverageMode( CEvaluator::AVG_UNWEIGHTED );
-                std::cout << CEvaluator::StratifiedCrossValidation( &ila, *pDataSet );
+                std::cout << data.first << ",discrete"<<t<<",cross,u," << i <<",";
+                std::cout << CEvaluator::CrossValidation( pClassifier, *pDataSet);
                 CEvaluator::SetAverageMode( CEvaluator::AVG_GLOBAL );
-                std::cout << CEvaluator::StratifiedCrossValidation( &ila, *pDataSet );
+                std::cout << data.first << ",discrete"<<t<<",cross,g," << i <<",";
+                std::cout << CEvaluator::CrossValidation( pClassifier, *pDataSet );
+
+                CEvaluator::SetAverageMode( CEvaluator::AVG_WEIGHTED );
+                std::cout << data.first << ",discrete"<<t<<",stratified,w," << i <<",";
+                std::cout << CEvaluator::StratifiedCrossValidation( pClassifier, *pDataSet );
+                CEvaluator::SetAverageMode( CEvaluator::AVG_UNWEIGHTED );
+                std::cout << data.first << ",discrete"<<t<<",stratified,u," << i <<",";
+                std::cout << CEvaluator::StratifiedCrossValidation( pClassifier, *pDataSet );
+                CEvaluator::SetAverageMode( CEvaluator::AVG_GLOBAL );
+                std::cout << data.first << ",discrete"<<t<<",stratified,g," << i <<",";
+                std::cout << CEvaluator::StratifiedCrossValidation( pClassifier, *pDataSet );
 
             }
-        }
-        delete pDataSet;
 
+            delete pClassifier;
+        }
+
+        delete pDataSet;
     }
 
 //    CDataSet data;
